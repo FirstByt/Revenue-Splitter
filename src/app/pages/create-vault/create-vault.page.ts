304ddd432cd4909e582cd016f29543d6cbf3d567
Tirro <tirro-s@ukr.net>
2025-08-26 04:06:15 +0300

    mobile cleanup

--- src/app/pages/create-vault/create-vault.page.ts
+++ src/app/pages/create-vault/create-vault.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonToggle } from '@ionic/angular/standalone';
 
 import { CreateVaultProgressComponent } from 'src/app/components/create-vault-progress/create-vault-progress.component';
 import { HeaderComponent } from 'src/app/components/header/header.component';
@@ -10,10 +11,13 @@ import { WlletComponent } from 'src/app/components/wallet/wallet.component';
 @Component({
   templateUrl: 'create-vault.page.html',
   styleUrl: 'create-vault.page.scss',
-  imports: [ IonContent, HeaderComponent, CreateVaultProgressComponent, StepInfoComponent, RouterModule, WlletComponent ]
+  imports: [ IonContent, HeaderComponent, CreateVaultProgressComponent, StepInfoComponent, RouterModule, WlletComponent, IonToggle, FormsModule ]
 })
 
 export class CreateVaultPage implements OnInit {
+
+  isImMutable = false;
+
   constructor() { }
 
   ngOnInit() { }
